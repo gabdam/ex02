463bbedcd7fe290d27561d0598983540d7f20001
gabdam <gabidamo999@hotmail.com>
2014-09-12 16:34:51 -0400

    version corrige

--- bin/ex02.dart
+++ bin/ex02.dart
@@ -1,22 +1,24 @@
 void main() {
   // 1) Calculer a la main 5-7/8. R = -0.25
-  // Le résultat n'est pas le même!  -0.25 et 4.125
+  // Le résultat est le même!  -0.25
   int x = 5;
   int y = 7;
   int z = 8;
-  print((x-y)/z);
+  print("1) Cela donne ${(x-y)/z}.");
 
-  // 2) Calculer un cercle de rayon 10. R = 62.831852 unit²
-  var pi = 3.1415926;
-  var radius = 10;
-  print(pi*radius*radius);
+  // 2) Calculer un cercle de rayon 10. R = 314.15926 unit²
+  double pi = 3.1415926;
+  int radius = 10;
+  print("2) L'aire d'un cercle de rayon 10 est de ${pi*radius*radius} unités².");
 
   // 3) Convertir feet en acres. R = 1.31313 acres
+  int larg = 220;
+  int long = 260;
   var convertrate = 43560; // valeur de 1 acre en pied
-  print(220*260/convertrate);
+  print("3) L'aire du terrain est de ${long*larg/convertrate} acres");
   
   // 4) Vitesse de usain bolt, 100 metres en 9.58 secondes. R = 10.43841336 metres par seconde 
   var speed = 100/9.58*3.6;
-  print("Usain Bolt cours à une vitesse de $speed km/h!");
+  print("4) Usain Bolt cours à une vitesse de $speed km/h!");
 }
 
